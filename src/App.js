edb45d7b755cbc5b17dfa619aa61f1d46f302854
Julia Gao Miller <julia.gao.miller@gmail.com>
2021-11-14 19:04:35 +0700

    successfully displaying the airtable data

--- src/App.js
+++ src/App.js
@@ -8,19 +8,41 @@ const base = new Airtable({ apiKey: apiKey }).base(baseId);
 
 function App() {
 
+  const [entries, setEntries] = useState([]);
+
    //this method pulls in the data
    useEffect(() => {
     //pull in the 'goals' table view data
     base("entries")
       .select({ view: "Grid view" })
       .eachPage((records, fetchNextPage) => { 
-        console.log(records)
+        setEntries(records);
+        // console.log(records)
         fetchNextPage();
       });
   }, []);
 
+
+  let total = 0
+
   return (
-    <div>
+    <div style={{textAlign: "center"}}>
+      <h1>STRYK Work Log</h1>
+      <p>Total time worked: {total}
+
+      </p>
+      <h3>All Entries</h3>
+      {entries.map((entry, index) => (
+        <div key={index}>
+          <span>🗓{entry.fields.date} </span>
+          <span>🕐{entry.fields.minutes} min –– </span>
+          <span><i>#{entry.fields.tag}</i></span>
+          <br/>
+          <span><i>{entry.fields.description}</i></span>
+          <br/>
+          <br/>
+        </div>
+      ))}
     </div>
   );
 }
