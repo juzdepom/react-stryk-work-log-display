7b549ef7816b426ff65a4b0634f901c0ffc81575
Julia Gao Miller <julia.gao.miller@gmail.com>
2021-11-14 17:50:06 +0700

    successfully pulling (but not displaying) the data from airtable

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
+// import the airtable ids
+import { apiKey, baseId } from "./airtableIds";
+
+import React, { useEffect, useState } from "react";
+import Airtable from "airtable";
+
+const base = new Airtable({ apiKey: apiKey }).base(baseId);
+
 function App() {
+
+   //this method pulls in the data
+   useEffect(() => {
+    //pull in the 'goals' table view data
+    base("entries")
+      .select({ view: "Grid view" })
+      .eachPage((records, fetchNextPage) => { 
+        console.log(records)
+        fetchNextPage();
+      });
+  }, []);
+
   return (
     <div>
     </div>
